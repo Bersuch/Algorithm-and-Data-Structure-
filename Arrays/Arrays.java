3888f3603c4ff950bf44d646d5ede28bd2b0f722
Daniel Bersuch <bersuchdaniel@yahoo.de>
2021-07-31 01:05:52 +0200

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -18,7 +18,8 @@ public class Arrays {
 		double max = a[0];
 		for(int i = 1; i < a.length; i++) {
 			if(a[i] > max) {
-				max = a[i];			}
+				max = a[i];			
+			}
 		}
 		
 		// Calculate average
